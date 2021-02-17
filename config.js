594bb6695c474e79604da6d142570221494a722e
Jeisson Francois Rosas Cespedes <jefraroce@hotmail.com>
2021-02-17 18:32:44 -0500

    Updated default environment variables at config.js file.

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 3000,
-  MONGODB: process.env.MONGODB || 'mongodb://localhost:27017/bit-cenisoft-bookstore'
+  MONGODB: process.env.MONGODB || 'mongodb://localhost:27017/bit-cenisoft-bookstore',
+  SECRET: process.env.SECRET || 'misecreto',
+  DROPBOX_TOKEN: process.env.DROPBOX_TOKEN || ''
 }
